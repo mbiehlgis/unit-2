8fad564a617e894b983a8f104a9284ea2ce79ca4
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-02-12 15:18:33 -0600

    Update geojsonTutorial.js

---
+++ js/tutorials/geojsonTutorial.js
@@ -0,0 +1,73 @@
+var geojsonFeature = {
+    "type": "Feature",
+    "properties": {
+        "name": "Coors Field",
+        "amenity": "Baseball Stadium",
+        "popupContent": "This is where the Rockies play!"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-104.99404, 39.75621]
+    }
+}; //sample geojsonFeature
+
+L.geoJSON(geojsonFeature).addTo(map); //l.geoJSON creates a geojson layer and adds it to the map
+
+var myLines = [{
+    "type": "LineString",
+    "coordinates": [[-100, 40], [-105, 45], [-110, 55]]
+}, {
+    "type": "LineString",
+    "coordinates": [[-105, 40], [-110, 45], [-115, 55]]
+}]; // passing an array of geoJSON objects via a variable
+
+// var myLayer = L.geoJSON().addTo(map);
+// myLayer.addData(geojsonFeature); // create an empty geojson layer to add features to it later
+
+var myStyle = {
+    "color": "#ff7800",
+    "weight": 5,
+    "opacity": 0.65
+};
+
+L.geoJSON(myLines, {
+    style: myStyle // ^^uses one variable to style both lines and polygons the same way
+}).addTo(map);
+
+// code below shows how I can style via the example "party" property in this geoJSON
+var states = [{
+    "type": "Feature",
+    "properties": {"party": "Republican"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-104.05, 48.99],
+            [-97.22,  48.98],
+            [-96.58,  45.94],
+            [-104.03, 45.94],
+            [-104.05, 48.99]
+        ]]
+    }
+}, {
+    "type": "Feature",
+    "properties": {"party": "Democrat"},
+    "geometry": {
+        "type": "Polygon",
+        "coordinates": [[
+            [-109.05, 41.00],
+            [-102.06, 40.99],
+            [-102.03, 36.99],
+            [-109.04, 36.99],
+            [-109.05, 41.00]
+        ]]
+    }
+}];
+
+L.geoJSON(states, {
+    style: function(feature) {
+        switch (feature.properties.party) {
+            case 'Republican': return {color: "#ff0000"};
+            case 'Democrat':   return {color: "#0000ff"};
+        }
+    }
+}).addTo(map);
