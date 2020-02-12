7c1e9d6db7aac59ee1ee06ae3d508e3b23a292d3
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-02-12 15:18:35 -0600

    Update quickstartTutorial.js

--- js/tutorials/quickstartTutorial.js
+++ js/tutorials/quickstartTutorial.js
@@ -16,3 +16,24 @@ var circle = L.circle([51.508, -0.11], {
     fillOpacity: 0.5,
     radius: 500
 }).addTo(mymap); // adds circle polygon over area
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap); //adding a polygon, each set of coordinates is a point to connect to
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon."); //easy pop-up shortcut, "bindPopup" adds popup text to each variable name
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap); // standalone popup separate from any object, popup locaion set on coordinates
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
