8ced0e82909c74cb2998da92c18ec53b04cb6043
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-02-17 15:24:36 -0600

    Update quickstartTutorial.js

--- js/tutorials/quickstartTutorial.js
+++ js/tutorials/quickstartTutorial.js
@@ -6,32 +6,32 @@ var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest
 
 Esri_WorldImagery.addTo(mymap);
 
-// var marker = L.marker([51.5, -0.09]).addTo(mymap); // adds marker to map
-//
-// var circle = L.circle([51.508, -0.11], {
-//     color: 'red',
-//     fillColor: '#f03',
-//     fillOpacity: 0.5,
-//     radius: 500
-// }).addTo(mymap); // adds circle polygon over area
-//
-// var polygon = L.polygon([
-//     [51.509, -0.08],
-//     [51.503, -0.06],
-//     [51.51, -0.047]
-// ]).addTo(mymap); //adding a polygon, each set of coordinates is a point to connect to
-//
-// marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-// circle.bindPopup("I am a circle.");
-// polygon.bindPopup("I am a polygon."); //easy pop-up shortcut, "bindPopup" adds popup text to each variable name
-//
-// var popup = L.popup()
-//     .setLatLng([51.5, -0.09])
-//     .setContent("I am a standalone popup.")
-//     .openOn(mymap); // standalone popup separate from any object, popup locaion set on coordinates
-//
-// function onMapClick(e) {
-//     alert("You clicked the map at " + e.latlng);
-// }
-//
-// mymap.on('click', onMapClick);
+var marker = L.marker([51.5, -0.09]).addTo(mymap); // adds marker to map
+
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap); // adds circle polygon over area
+
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap); //adding a polygon, each set of coordinates is a point to connect to
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon."); //easy pop-up shortcut, "bindPopup" adds popup text to each variable name
+
+var popup = L.popup()
+    .setLatLng([51.5, -0.09])
+    .setContent("I am a standalone popup.")
+    .openOn(mymap); // standalone popup separate from any object, popup locaion set on coordinates
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+mymap.on('click', onMapClick);
