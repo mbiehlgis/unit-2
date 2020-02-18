3ca58ea6cdf6862db51df81bbf6134b18e4d9fa6
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-02-18 05:48:02 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,66 @@
-// test
+var map;
+
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    map = L.map('mapid', {
+        center: [36.20, 138.25],
+        zoom: 2,
+        minZoom: 4
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    	subdomains: 'abcd',
+    	maxZoom: 19
+    }).addTo(map);
+
+    //call getData function
+    getData();
+};
+
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+        }
+        layer.bindPopup(popupContent);
+    };
+};
+
+//function to retrieve the data and place it on the map
+function getData(){
+    //load the data
+    $.getJSON("data/JapanPop.geojson", function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 4,
+                fillColor: "#95003A",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+
+            //adding circle markers to map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                },
+                onEachFeature: onEachFeature
+            }).addTo(map);
+
+            // applying popup function to geojson data
+           //  L.geoJson(response, {
+           //     onEachFeature: onEachFeature
+           // }).addTo(map);
+        });
+};
+
+$(document).ready(createMap);
