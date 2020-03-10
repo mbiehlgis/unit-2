7502e8aa4b5118c6bb9dd649c7c01bab576debfa
mbiehlgis <60198329+mbiehlgis@users.noreply.github.com>
2020-03-10 13:10:58 -0500

    Fixed Zoom Controls and Color Swap
    - Attributes Legend still in progress
    - Massive Color Palette Overhaul

--- js/main.js
+++ js/main.js
@@ -7,19 +7,26 @@ var dataStats = {}; //making a global variable list to append max, mean, and min
 function createMap(){
 
     map = L.map('mapid', {
-        center: [36.20, 138.25],
+        center: [36.20, 136.25],
         zoom: 2,
-        minZoom: 4.4,
+        minZoom: 5,
         maxZoom: 8,
         maxBounds: [(25.2, 115.7), (45.7, 155.54)],
     });
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
-    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    	subdomains: 'abcd',
-    	maxZoom: 30
+    // L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+    // 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    // 	subdomains: 'abcd',
+    // 	maxZoom: 30
+    // }).addTo(map);
+    // map.zoomControl.setPosition('bottomright');
+    // getData();
+
+    L.tileLayer('https://{s}.tile.openstreetmap.se/hydda/base/{z}/{x}/{y}.png', {
+	    maxZoom: 30,
+	    attribution: 'Tiles courtesy of <a href="http://openstreetmap.se/" target="_blank">OpenStreetMap Sweden</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
-    map.zoomControl.setPosition('bottomright');
+    map.zoomControl.setPosition('topright');
     getData();
 };
 
@@ -172,7 +179,7 @@ function pointToLayer(feature, latlng, attributes){
     var attribute = attributes[0];
 
     var geojsonMarkerOptions = {
-        fillColor: "#95003A",
+        fillColor: "#FF69B4",
         color: "#000",
         weight: 1,
         opacity: 1,
@@ -279,24 +286,3 @@ function getData(map){
 };
 
 $(document).ready(createMap);
-
-
-// ---- back to top button ----
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
